ecf7ed68c8e5bd6d5581fdb71eb5b853c8f731d2
ian8829 <ghjkghj@naver.com>
2021-07-19 13:54:27 +0900

    add init

---
+++ Grids/Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by ian white on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
