f703da4e87709f501b12f05b112f8e452efbb893
ian8829 <ghjkghj@naver.com>
2021-07-19 14:00:29 +0900

    use lazyGrid

--- Grids/Grids/ContentView.swift
+++ Grids/Grids/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let data = Array(1...1000).map { "Item \($0)" }
+    
+    let layout = [
+        GridItem(.adaptive(minimum: 80))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: layout, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+                ForEach(data, id: \.self) {
+                    item in Text(item)
+                }
+            }
+        }
     }
 }
 
